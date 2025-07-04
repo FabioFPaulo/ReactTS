820759ce20c1fee99b47e47c3e673eace3d4723f
Fábio Fraga Paulo <fabiofrapaulo@outlook.pt>
2025-07-04 21:30:12 +0100

    QuoteForge init project

---
+++ QuoteForge/vite.config.ts
@@ -0,0 +1,18 @@
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src/"),
+            "@components": path.resolve(__dirname, "./src/components/"),
+            "@assets": "./src/assets/",
+            "@hooks": "./src/hooks/",
+            "@screens": "./src/screens/",
+            "@repositories": path.resolve(__dirname, "./src/repositories"),
+        },
+    },
+});
