b1378e33456973983029bbb0fe8e88a65977a600
Fábio Fraga Paulo <fabiofrapaulo@outlook.pt>
2026-01-24 20:00:13 +0000

    footer

--- MacbookGsapApp/src/components/Footer.tsx
+++ MacbookGsapApp/src/components/Footer.tsx
@@ -1,5 +1,30 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer>
+            <div className="info">
+                <p>
+                    More wait to shop: Find an Apple Store or other retailer
+                    near you. Or call 000000 000 0000
+                </p>
+                <img src="/logo.svg" alt="Apple logo" />
+            </div>
+
+            <hr />
+
+            <div className="links">
+                <p></p>
+                <ul>
+                    {footerLinks.map((link) => (
+                        <li key={link.label}>
+                            <a href={link.link}>{link.label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
